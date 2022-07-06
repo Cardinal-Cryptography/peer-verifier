46331b32aa0866d1dfd65cc53153ede9f9712abb
maciejnems <macieknems@gmail.com>
2022-07-06 15:53:17 +0200

    Add no message handling to docker entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-signer --message "$PEER_ID" --p2p-secret-path "$P2P_SECRET_PATH"
+if [[ -z $MESSAGE ]]
+then
+    signer --p2p-secret-path "$P2P_SECRET_PATH"
+else
+    signer --message "$MESSAGE" --p2p-secret-path "$P2P_SECRET_PATH"
+fi
 
 echo "Done!"
